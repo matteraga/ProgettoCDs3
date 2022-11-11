6ef45f0b52d2ea11894c68869008cc8d1691e590
Matteo <matteoraganini@gmail.com>
2022-11-11 14:22:42 +0100

    Aggiunta classe Brano

---
+++ Brano.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgettoCDs3
+{
+    internal class Brano
+    {
+
+        private string titolo;
+        private string autore;
+        private double durata;
+
+        public Brano(string titolo, string autore, double durata)
+        {
+            this.titolo = titolo;
+            this.autore = autore;
+            this.durata = durata;
+        }
+
+        public string getTitolo()
+        {
+            return titolo;
+        }
+
+        public string getAutore()
+        {
+            return autore;
+        }
+
+        public double getDurata()
+        {
+            return durata;
+        }
+
+        public void setTitiolo(string titolo)
+        {
+            this.titolo = titolo;
+        }
+
+        public void setAutore(string autore)
+        {
+            this.autore = autore;
+        }
+
+        public void setDurata(double durata)
+        {
+            this.durata = durata;
+        }
+
+        public string toString()
+        {
+            return $"Titiolo: {titolo}\nAutore: {autore}\nDurata: {durata}";
+        }
+
+        public bool shortSong(double durata)
+        {
+            return durata > this.durata;
+        }
+    }
+}
